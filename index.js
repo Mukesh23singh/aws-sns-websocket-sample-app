d51afd450e082ad5a190310458862db4eff38a26
Mukesh <Mukesh23.singh@gmail.com>
2017-06-09 08:59:06 +0530

    third

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function(req, res, next){
-  res.sendfile(__dirname + '/public/index.html');
+
 });
 
 app.post('/', function(req, res, next){

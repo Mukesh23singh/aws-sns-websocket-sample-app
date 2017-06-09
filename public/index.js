ee4716623e5895fc3ec87de7d606ec99d8595b64
Mukesh <Mukesh23.singh@gmail.com>
2017-06-09 08:53:48 +0530

    intial

---
+++ public/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var expressWs = require('express-ws')(app);
+
+app.use(function (req, res, next) {
+  console.log('middleware');
+  req.testing = 'testing';
+  return next();
+});
+
+app.get('/', function(req, res, next){
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+app.post('/', function(req, res, next){
+  console.log('get route', req.testing);
+  res.end();
+});
+
+function sendNotification(req){
+  ws.send(req.msg);
+}
+
+app.listen(3000);

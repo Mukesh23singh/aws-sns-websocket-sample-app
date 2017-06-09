c8e566b157b15f6eb680512c6253d95d3889f6b2
Mukesh <Mukesh23.singh@gmail.com>
2017-06-09 10:31:06 +0530

    sixth

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var url = require('url');
 var WebSocket = require('ws');
 var app = express();
 var server = http.createServer(app);
-var wss = new WebSocket.Server({ server });
+var wss = new WebSocket.Server({ server: "https://shrouded-dawn-79681.herokuapp.com/", port: "1337" });
 console.log(JSON.stringify(wss))
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
